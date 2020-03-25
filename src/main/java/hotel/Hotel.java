5f3af6c7db53a0f17ed0782b7c010837f4700be8
Númi Steinn Baldursson <numisteinn@gmail.com>
2020-03-25 20:47:56 +0000

    okei, þetta virkar fyrir mig

--- src/main/java/hotel/Hotel.java
+++ src/main/java/hotel/Hotel.java
@@ -10,7 +10,8 @@ public class Hotel extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("adal.fxml"));
+
+        Parent root = FXMLLoader.load(getClass().getResource("/adal.fxml"));
         primaryStage.setTitle("Hello World");
         primaryStage.setScene(new Scene(root));
         primaryStage.show();
